e963e218511326f24d57f9e2e4ab7aab08ac7d2e
yonggyo1125 <yonggyo00@kakao.com>
2023-09-22 19:12:15 +0900

    사이트 기본 설정 중

--- src/main/java/org/project/configs/MvcConfig.java
+++ src/main/java/org/project/configs/MvcConfig.java
@@ -1,15 +1,35 @@
 package org.project.configs;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Slf4j
 @Configuration
 @EnableConfigurationProperties(FileConfig.class)
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private FileConfig fileConfig;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+       registry.addResourceHandler(fileConfig.getUrl() + "**")
+               .addResourceLocations("file:///" + fileConfig.getPath());
+    }
 
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+        ms.setBasenames("messages.commons", "messages.validations", "messages.errors");
+        ms.setDefaultEncoding("UTF-8");
+
+        return ms;
     }
 }
