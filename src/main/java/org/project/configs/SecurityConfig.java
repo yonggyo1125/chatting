cd8f9e47d1aa6e35fbed1ea416cb8dbb3c058d4d
yonggyo1125 <yonggyo00@kakao.com>
2023-09-26 19:11:38 +0900

    Cors 설정

--- src/main/java/org/project/configs/SecurityConfig.java
+++ src/main/java/org/project/configs/SecurityConfig.java
@@ -1,17 +1,26 @@
 package org.project.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private CorsFilter corsFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable())
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class);
+
 
         return http.build();
     }
