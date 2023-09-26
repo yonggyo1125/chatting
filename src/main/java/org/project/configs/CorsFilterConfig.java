244842af7c338ca881dbf20256d64a5b82f609b0
yonggyo1125 <yonggyo00@kakao.com>
2023-09-26 19:04:14 +0900

    Cors 설정

---
+++ src/main/java/org/project/configs/CorsFilterConfig.java
@@ -0,0 +1,16 @@
+package org.project.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsFilterConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+    }
+}
