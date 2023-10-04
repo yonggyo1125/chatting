ae5ea9c22e26f9f480a3e45abb5c8dd94999f0e9
yonggyo1125 <yonggyo00@kakao.com>
2023-10-04 19:34:39 +0900

    프론트 앱 구성

---
+++ front_app/chatting_fe/src/pages/Join.js
@@ -0,0 +1,5 @@
+const Join = () => {
+    return <h1>회원가입</h1>;
+}
+
+export default Join;
---
+++ front_app/chatting_fe/src/styles/fontSizes.js
@@ -0,0 +1,6 @@
+export default {
+    small : '0.75rem', 
+    normal : '1rem', 
+    medium: '1.5rem', 
+    big: '2rem'
+};
