6f9e454c3ba3cc4362195401cd993da345a0f7e1
yonggyo1125 <yonggyo00@kakao.com>
2023-10-04 20:52:31 +0900

    프론트 앱 구성

--- front_app/chatting_fe/src/pages/Login.js
+++ front_app/chatting_fe/src/pages/Login.js
@@ -1,13 +1,40 @@
-import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import ErrorMessage from '../components/commons/ErrorMessage';
 
 const Login = () => {
-    const isLogin = true;
-    const navigate = useNavigate();
-    if (isLogin) {
-        navigate("/");
-    }
+    const [form, setForm] = useState({
+        userId : '',
+        userPw : '',
+    });
 
-    return <h1>로그인</h1>;
+    const [message, setMessage] = useState('');
+
+    const handleChange = (e) => {
+        form[e.target.name] = e.target.value;
+        setForm({ ...form});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!form.userId.trim()) {
+            setMessage("아이디를 입력하세요.");
+            return;
+        }
+
+        if (!form.userPw.trim()) {
+            setMessage("비밀번호를 입력하세요.");
+            return;
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="text" name="userId" placeholder="아이디" onChange={handleChange} /><br />
+            <input type="password" name="userPw" placeholder="비밀번호" onChange={handleChange} /><br />
+            { message && <ErrorMessage>{message}</ErrorMessage>}
+            <button type="submit">로그인</button>
+        </form>
+    )
 };
 
 export default Login;
