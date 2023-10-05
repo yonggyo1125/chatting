4ba73535be2819535d5533d05a40f640a2292126
yonggyo1125 <yonggyo00@kakao.com>
2023-10-05 20:42:31 +0900

    프론트 앱 구성

---
+++ front_app/chatting_fe/src/api/chatting.js
@@ -0,0 +1,10 @@
+import apiRequest from "../lib/apiRequest";
+
+/** 방목록 */
+export const getRooms = () => apiRequest("/rooms");
+
+/** 방등록 */
+export const registerRoom = (form) => apiRequest("/room", "POST", form); 
+
+/** 채팅 메세지 기록 */
+export const registerMessage = (form) => apiRequest("/message", "POST", form);
--- front_app/chatting_fe/src/pages/Rooms.js
+++ front_app/chatting_fe/src/pages/Rooms.js
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import { getRooms } from "../api/chatting";
+import ErrorMessage from '../components/commons/ErrorMessage';
 
 const Rooms = () => {
-    
+    const [rooms, setRooms] = useState([]);
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+        getRooms()
+            .then((data) => {
+                console.log(data);
+                setRooms(data)
+            })
+            .catch((err) => {
+                console.error(err);
+                setMessage("방목록 조회 실패...");
+            });
+    }, []);
+
     return (
         <>
+           {message && <ErrorMessage>{message}</ErrorMessage>}
            
         </>
     );
