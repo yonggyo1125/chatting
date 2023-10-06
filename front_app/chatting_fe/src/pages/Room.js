fcf636f35eb735b4617ccc814d5e5455b07e04a9
yonggyo1125 <yonggyo00@kakao.com>
2023-10-06 20:45:23 +0900

    .

--- front_app/chatting_fe/src/pages/Room.js
+++ front_app/chatting_fe/src/pages/Room.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import Title from "../components/commons/Title";
 import { getRoom } from "../api/chatting";
 
+let webSocket;
 const Room = () => {
     const initialInfo = {
         roomNo : '',
@@ -10,11 +11,33 @@ const Room = () => {
         max : ''
     };
     const { roomNo } = useParams();
-    const { roomInfo, setRoomInfo } = useState(initialInfo);
+    const [ roomInfo, setRoomInfo ] = useState(initialInfo);
+    
+    useEffect(() => {
+        webSocket = new WebSocket(process.env.REACT_APP_WS_URL);
+        webSocket.onopen = (e) => {
+            console.log("연결 성공");
+        };
+        
+        webSocket.onclose = (e) => {
+            console.log("연결 종료");
+        };
+
+        webSocket.onmessage = (data) => {
+            console.log(data);
+        };
+
+        getRoom(roomNo)
+            .then((res) => setRoomInfo(res.data))
+            .catch((err) => console.error(err));
+    }, [])
+
+   
+  
 
     return (
-        <>
-            <Title>{roomInfo.roomNm}({roomInfo.max ? `최대${roomInfo.max}명`: '무제한'})</Title>
+        <>  
+            {roomInfo && <Title>{roomInfo.roomNm}({roomInfo.max ? `최대${roomInfo.max}명`: '무제한'})</Title>}
         </>
     );
 };
