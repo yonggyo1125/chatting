806e16e4c4e1b2730a234f59ccf1a9f158851079
yonggyo1125 <yonggyo00@kakao.com>
2023-10-10 21:28:27 +0900

    .

--- front_app/chatting_fe/src/pages/MainPage.js
+++ front_app/chatting_fe/src/pages/MainPage.js
@@ -1,5 +1,7 @@
+import { Navigate } from "react-router-dom";
+
 const MainPage = () => {
-    return <h1>메인페이지!</h1>;
+    return <Navigate to="/room" replace={true} />
 };
 
 export default MainPage;
